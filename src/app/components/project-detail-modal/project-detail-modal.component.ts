e884d44e94569aefe987a7edf63b21565d1ac17b
Devin Cooper <devinraycooper@gmail.com>
2025-07-04 14:15:58 -0400

    styled details pages

--- src/app/components/project-detail-modal/project-detail-modal.component.ts
+++ src/app/components/project-detail-modal/project-detail-modal.component.ts
@@ -1,21 +1,43 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'project-detail-modal',
-  imports: [CommonModule, MatDialogModule],
+  imports: [CommonModule, MatDialogModule, MatIcon, MatCardModule],
   templateUrl: './project-detail-modal.component.html',
   styleUrl: './project-detail-modal.component.scss'
 })
 export class ProjectDetailModalComponent {
+  buttonLabel: string = 'Install';
+
   constructor(
     public dialogRef: MatDialogRef<ProjectDetailModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
-  ) {}
+  ) {
+    if (data.website) this.buttonLabel = 'Go to site';
+  }
 
-  goTo(url:string) {
+  goToUrl(url:string) {
     window.open(url, '_blank');
+  }
+
+  goToApp(data: any) {
+    if (data.website) {
+      window.open(data.website, '_blank');
+    }
+
+    const userAgent = navigator.userAgent.toLowerCase();
+
+    if (userAgent.includes('android')) {
+      window.open(data.androidLink, '_blank')
+    } else if (userAgent.includes('iphone') || userAgent.includes('ipad') || userAgent.includes('ipod')) {
+      window.open(data.iosLink, '_blank');
+    } else {
+      window.open(data.webLink, '_blank');
+    }
   }
 
   close(): void {
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -4,6 +4,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { CommonModule } from '@angular/common';
 import { MatDialog } from '@angular/material/dialog';
 import { ProjectDetailModalComponent } from '../project-detail-modal/project-detail-modal.component';
+import projects from '../../../assets/data/projects.json';
 
 @Component({
   selector: 'app-projects',
@@ -12,58 +13,17 @@ import { ProjectDetailModalComponent } from '../project-detail-modal/project-det
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-  projects = [
-    {
-      name: 'Not Todo',
-      icon: 'assets/images/icons/not-todo.png',
-      description: 'The Not To-Do App is a mobile-first productivity tool designed to help users break bad habits by focusing on what they should avoid doing. Instead of tracking tasks to complete, it encourages mindful behavior by letting users create and categorize habits they want to eliminate. The app sends smart daily reminders, tracks streaks, and provides visual feedback on progress. Built with Ionic, Angular, and SQLite, the Not To-Do App combines clean design with intentional functionality to help users stay focused, disciplined, and in control of their time and attention.',
-      downloadUrl: 'https://play.google.com/store/apps/details?id=com.darcsoftware.petassistant&pcampaignid=web_share',
-      github: 'https://github.com/gymtechlaundry/NotTodo'
-    },
-    {
-      name: 'Pet Assistant',
-      icon: 'assets/images/icons/pet-assistant.png',
-      description: 'Under Construction',
-      downloadUrl: 'https://play.google.com/store/apps/details?id=com.darcsoftware.petassistant&pcampaignid=web_share',
-      github: 'https://github.com/gymtechlaundry/pet-assistant'
-    },
-    {
-      name: 'My Portfolio',
-      icon: 'assets/images/icons/portfolio.png',
-      description: 'Under Construction',
-      downloadUrl: 'https://play.google.com/store/apps/details?id=com.darcsoftware.petassistant&pcampaignid=web_share',
-      github: 'https://github.com/gymtechlaundry/pet-assistant'
-    },
-    {
-      name: 'DARC Software',
-      icon: 'assets/images/icons/darcsoftware.png',
-      description: 'Under Construction',
-      downloadUrl: 'https://play.google.com/store/apps/details?id=com.darcsoftware.petassistant&pcampaignid=web_share',
-      github: 'https://github.com/gymtechlaundry/pet-assistant'
-    },
-    {
-      name: 'My Github',
-      icon: 'assets/images/icons/github.png',
-      description: 'Link',
-      downloadUrl: '',
-      github: '',
-      url: 'https://github.com/gymtechlaundry'
-    },
-    {
-      name: 'My LinkedIn',
-      icon: 'assets/images/icons/linkedin.png',
-      description: 'Link',
-      downloadUrl: '',
-      github: '',
-      url: 'https://www.linkedin.com/in/devinraycooper/'
-    }
-  ]
+  projects: any[] = [];
 
   constructor(private dialog: MatDialog) {
   }
 
+  ngOnInit(): void {
+    this.projects = projects;
+  }
+
   goToProject(app: any) {
-    if (!app.downloadUrl) {
+    if (app.url) {
       window.open(app.url, '_blank')
     } else {
       this.dialog.open(ProjectDetailModalComponent, {
@@ -71,7 +31,7 @@ export class ProjectsComponent {
         width: '100dvw',
         maxWidth: '100dvw',
         height: '100dvh',
-        panelClass: 'full-screen-modal'
+        maxHeight: '100dvh',
       })
     }
   }

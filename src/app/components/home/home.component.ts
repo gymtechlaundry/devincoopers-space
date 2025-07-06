19c7b5017b167036d4edc3115c2b0d271f6e49d6
Devin Cooper <devinraycooper@gmail.com>
2025-07-06 14:53:16 -0400

    add click to scroll down

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatIcon } from '@angular/material/icon';
 import { HeroComponent
@@ -12,5 +12,14 @@ import { ProjectsComponent } from "../projects/projects.component";
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  @ViewChildren('sectionRef') sectionRefs!: QueryList<ElementRef>
+
   currentTitle: string = 'THE HERO YOU DIDN\'T THINK YOU NEEDED';
+
+  scrollToSection(ref: ElementRef | undefined) {
+    if (ref && ref.nativeElement) {
+      ref.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
